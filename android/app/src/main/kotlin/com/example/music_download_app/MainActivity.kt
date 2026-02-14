21c66149de4fcfd47bdeb72073d175578118ed0b
zxc05896 <github05896@gmail.com>
2026-02-14 07:01:32 +0200

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/music_download_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/music_download_app/MainActivity.kt
@@ -1,22 +1,8 @@
 package com.example.music_download_app
 
-import android.os.Build
-import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-
-    override fun onResume() {
-        super.onResume()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-        } else {
-            @Suppress("DEPRECATION")
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
-            )
-        }
-    }
+class MainActivity: FlutterActivity() {
+    // 🚀 لا نحتاج لأي كود هنا!
+    // Flutter v2 Embedding بيقوم بكل الشغل أوتوماتيكياً
 }
